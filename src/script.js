51550ae5aca49bf225a3f33475a940fe8f922ccf
hidin <hidingw@gmail.com>
2017-07-27 13:47:49 +0300

    27-07-2017
    First commit
    TODO^ fix clearInterval() in myImage.mouseover event

---
+++ src/script.js
@@ -0,0 +1,42 @@
+var images = 
+	["images/tea.jpg",
+	 "images/coffee.jpg",
+	 "images/tobacco.jpg"];
+
+var buttonBackward = document.getElementById("backward");
+var buttonForward = document.getElementById("forward");
+var myImage = document.getElementById("slider");
+var i = 0;
+
+function getImage(){
+	myImage.style.background = "url('"+images[i]+"') no-repeat";
+	myImage.style.backgroundSize = "cover";
+};
+
+document.addEventListener("DOMContentLoaded", getImage);
+
+var autoTimer = setInterval(function() {
+	(i < images.length-1) ? i++ : i=0;
+	getImage();
+}, 3500);
+
+myImage.mouseover = function() {
+	clearInterval(autoTimer);
+};
+
+myImage.mouseout = function() {
+	autoTimer = setInterval(function() {
+	(i < images.length-1) ? i++ : i=0;
+	getImage();
+	}, 3500);
+};
+
+buttonForward.onclick = function() {
+	(i < images.length-1) ? i++ : i=0;
+	getImage();
+};
+
+buttonBackward.onclick = function() {
+	(i > 0) ? i-- : (i = images.length-1);
+	getImage();
+};
